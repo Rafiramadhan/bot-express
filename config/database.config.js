b03c7ab392a61762d5b493079bed1375acc42208
rafiramadhan <rafi@kata.ai>
2020-04-24 01:22:38 +0700

    revise

--- config/database.config.js
+++ config/database.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    // url: 'mongodb://localhost:27017/db_testing'
-    url: 'mongodb+srv://bot_test:bot_test@r-cluster-j9gq3.gcp.mongodb.net/test'
+    url: 'mongodb://localhost:27017/db_testing'
+    // url: 'mongodb+srv://bot_test:bot_test@r-cluster-j9gq3.gcp.mongodb.net/test'
 }
